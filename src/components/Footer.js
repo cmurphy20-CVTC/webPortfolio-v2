e699d09ed1385aa5da4afd8197dec2c38d32789e
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-04-02 19:01:38 -0500

    inital project upload

---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Footer() {
+  return(
+    <section>
+
+      <div class="footerTitle">
+        <h1>Have a great day!</h1>
+      </div>
+      <footer class="footer">
+
+        <div class="footerIcons container-fluid">
+      
+          <a href="https://github.com/cmurphy20-CVTC" target="_blank"><i class="social-icon fab fa-github fa-4x"></i></a>
+          <a href="https://www.linkedin.com/in/casey-murphy-494b49114/" target="_blank"><i class="social-icon fab fa-linkedin fa-4x"></i></a>
+          <a href="mailto:cmurphymwdf@gmail.com" target="_blank"><i class="social-icon fas fa-envelope fa-4x"></i></a>
+         
+          <p>Made by me © 2022</p>
+        </div>
+
+      </footer>
+
+      
+      <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+
+
+      
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></link>
+
+    </section>
+  )
+}
+
+export default Footer;
