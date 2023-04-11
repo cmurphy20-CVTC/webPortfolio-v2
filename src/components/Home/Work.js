db8ba4e6cf8451d1d8eb16b44a65918083f6ca88
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-04-10 19:06:09 -0500

    added link to video demo of Life Note

--- src/components/Home/Work.js
+++ src/components/Home/Work.js
@@ -148,6 +148,12 @@ function Work() {
                 </button>
               </a>
 
+              <a href="https://www.youtube.com/watch?v=XM6DNkFl9-E" target="_blank" rel="noreferrer">
+                <button id="solidBtn" class="btn">
+                  Video Demo
+                </button>
+              </a>
+
             </div>
           </div>
 
