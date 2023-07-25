cee9fe7335c86a4078d7650e3015e5d974d661f1
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-07-24 22:48:58 -0500

    updated with headshot

--- src/components/Contact/ContactImg.js
+++ src/components/Contact/ContactImg.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../../assets/Images/person.png";
+import Headshot from "../../assets/Images/headshot.png"
 
 function ContactImg() {
   return(
     <div class="row justify-content-center mt-4">
 
       <div class="row logo justify-content-center">
-        <img id="logoPerson" src={logo} alt=""></img>
+        <img id="logoPerson" src={Headshot} alt=""></img>
       </div>
 
     </div>
